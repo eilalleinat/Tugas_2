11f623cd7ddcba77a6e1d4d010651da31f70aa55
eilalleinat <eilalleinat@gmail.com>
2023-09-17 23:02:51 +0700

    Correcting some errors and variable names

--- main/urls.py
+++ main/urls.py
@@ -6,10 +6,10 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('home', show_home, name='show_home'),
-    path('create-product', create_product, name='create_product'),
+    path('create-item', create_item, name='create_item'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
 
 ]
