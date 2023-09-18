695ea08fd2f61d945d722e229e63fc5f39c932db
eilalleinat <eilalleinat@gmail.com>
2023-09-18 10:58:09 +0700

    another revision, on show html

--- main/views.py
+++ main/views.py
@@ -48,6 +48,10 @@ def show_xml(request):
     data = Item.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
+def show_html(request):
+    data = Item.objects.all()
+    return render(request, "show_html.html", {"data" : data})
+
 def show_json(request):
     data = Item.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
