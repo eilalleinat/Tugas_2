497ef65e407bd1071ee522557ab65091c5f9e236
eilalleinat <eilalleinat@gmail.com>
2023-10-03 15:48:38 +0700

    Tugas 4

--- main/views.py
+++ main/views.py
@@ -80,7 +80,6 @@ def delete_item(request, id):
 
 def edit_item(request, pk):
     item = get_object_or_404(Item, pk=pk)
-
     if request.method == 'POST':
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
