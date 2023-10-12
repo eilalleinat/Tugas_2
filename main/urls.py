1d29a2ecf51c30bfe971eeda3e1ee3a31162e071
eilalleinat <eilalleinat@gmail.com>
2023-10-12 17:29:36 +0700

    Selesai tugas 6 + bonus

--- main/urls.py
+++ main/urls.py
@@ -6,15 +6,22 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-item', create_item, name='create_item'),
-    path('edit-item/<int:pk>/', edit_item, name='edit_item'),
+    path('edit-item/<int:id>', edit_item, name='edit_item'),
+
     path('xml/', show_xml, name='show_xml'),
     path('html/', show_html, name='show_html'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('delete/<int:id>', delete_item, name='delete_item'),
 
+    path('delete/<int:id>', delete_item, name='delete_item'),
+    
+    path('get-item/', get_item_json, name='get_item_json'),
+    path('create-item-ajax/', add_item_ajax, name='add_item_ajax'),
+    path('delete_item_ajax/', delete_item_ajax, name='delete_item_ajax'),
+    path('edit_item_ajax/', edit_item_ajax, name='edit_item_ajax'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from main.forms import ItemForm
 from main.models import Item
 import datetime
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 def register(request):
@@ -78,17 +81,21 @@ def delete_item(request, id):
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
 
-def edit_item(request, pk):
-    item = get_object_or_404(Item, pk=pk)
-    
-    if request.method == 'POST':
-        form = ItemForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('main:show_main'))
-    else:
-        form = ItemForm(instance=item)
-    return render(request, 'edit_item.html', {'form': form, 'item': item})
+def edit_item(request, id):
+    # Get product berdasarkan ID
+    item = Item.objects.get(pk = id)
+
+    # Set item sebagai instance dari form
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_item.html", context)
+
 
 def show_xml(request):
     data = Item.objects.all()
@@ -101,6 +108,64 @@ def show_html(request):
 def show_json(request):
     data = Item.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def get_item_json(request):
+    item = Item.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', item))
+
+@csrf_exempt
+def add_item_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_item = Item(name=name, amount=amount, description=description, user=user)
+        new_item.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def delete_item_ajax(request):
+    if request.method == 'DELETE':
+        item_id = request.GET.get('id')
+        if item_id is not None:
+            try:
+                item = Item.objects.get(pk=item_id)
+                item.delete()
+                return HttpResponse(b'DELETED', status=200)
+            except Item.DoesNotExist:
+                return HttpResponse(b'Item not found', status=404)
+        else:
+            return HttpResponse(b'Invalid request', status=400)
+    else:
+        return HttpResponseNotFound()
+
+@csrf_exempt
+def edit_item_ajax(request):
+    if request.method == 'POST':
+        item_id = request.POST.get('id')
+        name = request.POST.get("name")
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+
+        if item_id is not None:
+            try:
+                item = Item.objects.get(pk=item_id, user=request.user)
+                item.name = name
+                item.amount = amount
+                item.description = description
+                item.save()
+                return HttpResponse(b'UPDATED', status=200)
+            except Item.DoesNotExist:
+                return HttpResponse(b'Item not found', status=404)
+        else:
+            return HttpResponse(b'Invalid request', status=400)
+    else:
+        return HttpResponseNotFound()
 
 def show_xml_by_id(request, id):
     data = Item.objects.filter(pk=id)
