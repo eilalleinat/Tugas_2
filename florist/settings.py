b9733275fabbef5063dce5f5fee6b11083c57993
eilalleinat <eilalleinat@gmail.com>
2023-11-22 10:56:03 +0700

    tugas 9

--- florist/settings.py
+++ florist/settings.py
@@ -55,6 +55,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://taniella-tugas.pbp.cs.ui.ac.id"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
